905a6de4b1e35dc6221c3796970f61bbaca92a7b
AnActualEmerald <emerald_actual@proton.me>
2022-09-08 12:14:17 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod api;
+pub mod core;
+pub mod error;
+pub mod model;
+
+// Re-exports
+pub use directories::ProjectDirs;
+
+// Important functions and structs
+pub use crate::core::utils::update_index;
+pub use crate::core::{get_outdated, install, update};
+pub use crate::model::{LocalIndex, LocalMod, Mod};
